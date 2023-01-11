5a97de2a018bdc97a7a92ea6f6d4864f1fe608e6
Никита <pravoepredserdie@gmail.com>
2023-01-11 18:13:41 +0300

    Authorization form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,55 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import Login from './Pages/Login';
 import NotFoundPage from './Pages/NotFoundPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App = () => (
-  <Routes>
-    <Route path="/" element={<div>hello world</div>} />
-    <Route path="/login" element={<Login />} />
-    <Route path="*" element={<NotFoundPage />} />
-  </Routes>
-);
+const App = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  const navigate = useNavigate();
+
+  const userLogin = (data) => {
+    axios.post('/api/v1/login', data)
+      .then((res) => {
+        const jwt = res.data.token;
+        if (jwt) {
+          setIsLogin(true);
+          navigate('/');
+          localStorage.setItem('jwt', jwt);
+        }
+        return res.data;
+      });
+  };
+
+  const checkToken = () => {
+    const jwt = localStorage.getItem('jwt');
+    if (jwt) {
+      setIsLogin(true);
+      navigate('/');
+    } else {
+      setIsLogin(false);
+      navigate('/login');
+    }
+  };
+
+  useEffect(() => {
+    checkToken();
+    console.log(isLogin);
+  }, []);
+
+  useEffect(() => {
+    console.log(isLogin);
+  }, [isLogin]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<div>hello world</div>} />
+      <Route path="/login" element={<Login userLogin={userLogin} />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+};
 
 export default App;
--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,53 +1,71 @@
+/* eslint-disable react/no-children-prop */
 import React from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
+// import { Button } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
 
-const Login = () => {
+const Login = ({ userLogin }) => {
   const formik = useFormik({
     initialValues: {
-      userName: '',
+      username: '',
       password: '',
     },
     validationSchema: Yup.object({
-      userName: Yup.string()
-        .required('Required'),
+      username: Yup.string()
+        .required('Имя пользователя обязательно для заполнения'),
       password: Yup.string()
-        .min(6, 'Must be 6 characters or more')
-        .required('Required'),
+        .min(5, 'Длина пароля должна быть не менее 5 символов')
+        .required('Пароль обязателен для заполнения'),
     }),
     onSubmit: (values) => {
-      console.log(JSON.stringify(values, null, 2));
+      userLogin(values);
     },
   });
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="userName">
-        Имя пользователя
-        <input
-          id="userName"
-          name="userName"
-          type="text"
-          value={formik.values.userName}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-      </label>
-      {formik.touched.userName && formik.errors.userName && (
-        <div>{formik.errors.userName}</div>)}
-      <label htmlFor="password">
-        Пароль
-        <input
-          id="password"
-          name="password"
-          type="password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-      </label>
-      {formik.touched.password && formik.errors.password && (
-        <div>{formik.errors.password}</div>)}
-    </form>
+    <Container fluid>
+      <div className="row justify-content-center align-content-center h-100">
+        <div className="col-12 col-md-8 col-xxl-6">
+          <Card className="shadow-sm">
+            <Card.Body className="p-5">
+              <Form onSubmit={formik.handleSubmit}>
+                <Form.Group className="mb-3" id="username">
+                  <Form.Label>Имя пользователя</Form.Label>
+                  <Form.Control
+                    id="username"
+                    name="username"
+                    type="text"
+                    placeholder="Введите имя пользователя"
+                    value={formik.values.username}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+                  {formik.touched.username && formik.errors.username && (
+                  <Form.Text className="text-danger">{formik.errors.username}</Form.Text>)}
+                </Form.Group>
+                <Form.Group className="mb-3" id="password">
+                  <Form.Label>Пароль</Form.Label>
+                  <Form.Control
+                    id="password"
+                    name="password"
+                    type="password"
+                    placeholder="Введите пароль"
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+                  {formik.touched.password && formik.errors.password && (
+                  <Form.Text className="text-danger">{formik.errors.password}</Form.Text>)}
+                </Form.Group>
+                <Form.Control type="submit" className="btn btn-primary" value="Войти" />
+              </Form>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+    </Container>
   );
 };
 
